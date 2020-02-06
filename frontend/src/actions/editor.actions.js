108cab689c50ae2369f7e01c09aea80d58a1bea4
raulojeda22 <raulojeda10g@gmail.com>
2020-02-06 16:15:50 +0100

    You can now run python code from the react client

---
+++ frontend/src/actions/editor.actions.js
@@ -0,0 +1,29 @@
+import { editorConstants } from '../constants';
+import { editorService } from '../services';
+import { alertActions } from './';
+
+export const editorActions = {
+    execute
+};
+
+function execute(value) {
+    return dispatch => {
+        dispatch(request({ value }));
+
+        editorService.execute(value)
+            .then(
+                value => {
+                    dispatch(success(value));
+                    dispatch(alertActions.success("Executed"))
+                },
+                error => {
+                    dispatch(failure(error.toString()));
+                    dispatch(alertActions.error(error.toString()));
+                }
+            );
+    };
+
+    function request(value) { return { type: editorConstants.EDITOR_REQUEST, value } }
+    function success(value) { return { type: editorConstants.EDITOR_SUCCESS, value } }
+    function failure(error) { return { type: editorConstants.EDITOR_FAILURE, error } }
+}
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,2 +1,3 @@
 export * from './alert.constants';
+export * from './editor.constants';
 export * from './user.constants';
